f5d78137b2ccca963772cc7a0207978c506d7c46
Legoracer <maxivanec5@gmail.com>
2021-01-31 23:55:38 +0100

    ssl fix

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ var httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer({});
 
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
+    //proxyReq.setHeader('X-Special-Proxy-Header', 'foobar');
 });
 
 var server = http.createServer(function(req, res) {
   proxy.web(req, res, {
+    changeOrigin: true,
     target: 'https://squids-awesome-project.firebaseio.com/'
   });
 });
